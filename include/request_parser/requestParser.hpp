84e69e4a8fd5b029137d2369099dbc666791abb1
kungbi <kipper12335@gmail.com>
2025-03-07 10:29:28 +0900

    Delete: Requests 클래스 삭제

--- include/request_parser/requestParser.hpp
+++ include/request_parser/requestParser.hpp
@@ -2,21 +2,22 @@
 # define REQUEST_PARSER_HPP
 
 # include <sstream>
+# include <string>
 
 class Request;  // forward declaration
 
 struct UriComponents {
-    std::string path;
-    std::string query;
-    std::string filename;
-    std::string extension;
+	std::string path;
+	std::string query;
+	std::string filename;
+	std::string extension;
 };
 
 class RequestParser {
-    public:
-        void parseRequestHeader(Request* request);
-        static UriComponents parseUri(const std::string& target);
-        void processHeader(Request* request, const std::string& header);
+public:
+	Request parseRequestHeader(const std::string originalRequest);
+	static UriComponents parseUri(const std::string& target);
+	void processHeader(const std::string& header, std::string& host, int& port, std::string& connection, size_t& contentLength, std::string& accept);
 };
 
 #endif
