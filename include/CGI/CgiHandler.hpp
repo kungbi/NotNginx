91763249dbfd047ed384a27a0eebf35468a3d67f
wonyocho <dnjsdud767@gmail.com>
2025-03-12 12:09:43 +0900

    [Refac] 중괄호 컨벤션에 맞게 수정

--- include/CGI/CgiHandler.hpp
+++ include/CGI/CgiHandler.hpp
@@ -4,17 +4,16 @@
 #include "Kqueue.hpp"
 #include "CgiExecuter.hpp"
 
-class CgiHandler
-{
-	private:
-		Kqueue& kqueue_;
-		CgiExecuter& cgiExecuter_;
-		
-		std::string requestTypeToString(RequestType type);
+class CgiHandler {
+private:
+	Kqueue& kqueue_;
+	CgiExecuter& cgiExecuter_;
+	
+	std::string requestTypeToString(RequestType type);
 
-	public:
-		CgiHandler(Kqueue& kqueue, CgiExecuter& cgiExecuter);
-		~CgiHandler();
-		
-		void handleRequest(const Request& request, int clientFd);
+public:
+	CgiHandler(Kqueue& kqueue, CgiExecuter& cgiExecuter);
+	~CgiHandler();
+	
+	void handleRequest(const Request& request, int clientFd);
 };
