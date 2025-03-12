f365fd168fbaf7749370764edf32dd0cd4593018
wonyocho <dnjsdud767@gmail.com>
2025-03-12 12:10:33 +0900

    [Refac] SGCHLD 함수 private 이동에 따른 내용 수정

--- src/CGI/CgiExecuter.cpp
+++ src/CGI/CgiExecuter.cpp
@@ -1,20 +1,5 @@
 #include "CgiExecuter.hpp"
 
-// `SIGCHLD` 핸들러: 종료된 자식 프로세스를 자동으로 수거
-void handleSigchld(int signo) {
-    (void)signo;
-    while (waitpid(-1, NULL, WNOHANG) > 0);
-}
-
-// 웹서버 시작 시 `SIGCHLD` 핸들러 설정
-void setupSigchldHandler() {
-    struct sigaction sa;
-    sa.sa_handler = handleSigchld;
-    sa.sa_flags = SA_RESTART | SA_NOCLDSTOP;
-    sigemptyset(&sa.sa_mask);
-    sigaction(SIGCHLD, &sa, NULL);
-}
-
 CgiExecuter::CgiExecuter() {
     setupSigchldHandler();
 }
@@ -28,20 +13,20 @@ int CgiExecuter::executeCgiScript(
     const std::string& requestBody
 ) {
     CgiRequestData requestData(scriptPath, queryString, requestMethod, requestBody);
-
+    
     int pipefd[2];
     if (pipe(pipefd) == -1)
-        throw std::runtime_error("Failed to create pipe");
-
+    throw std::runtime_error("Failed to create pipe");
+    
     pid_t pid = fork();
     if (pid == -1)
-        throw std::runtime_error("Failed to fork Cgi process");
-
+    throw std::runtime_error("Failed to fork Cgi process");
+    
     if (pid == 0) {
         executeChild(pipefd, requestData);
         exit(1);
     }
-
+    
     close(pipefd[1]);
     return pipefd[0];
 }
@@ -55,7 +40,7 @@ void CgiExecuter::executeChild(int pipefd[2], const CgiRequestData& requestData)
     if (requestData.requestMethod == "POST" || requestData.requestMethod == "PUT" || requestData.requestMethod == "PATCH") {
         setupBodyPipe(requestData.requestBody);
     }
-
+    
     execlp("python3", "python3", requestData.scriptPath.c_str(), NULL);
     perror("execlp");
     exit(1);
@@ -90,4 +75,20 @@ void CgiExecuter::setupBodyPipe(const std::string& requestBody) {
         throw std::runtime_error("Failed to redirect CGI input");
     }
     close(inputPipe[0]);
+
+}
+
+// `SIGCHLD` 핸들러: 종료된 자식 프로세스를 자동으로 수거
+void CgiExecuter::handleSigchld(int signo) {
+    (void)signo;
+    while (waitpid(-1, NULL, WNOHANG) > 0);
+}
+
+// 웹서버 시작 시 `SIGCHLD` 핸들러 설정
+void CgiExecuter::setupSigchldHandler() {
+    struct sigaction sa;
+    sa.sa_handler = CgiExecuter::handleSigchld;
+    sa.sa_flags = SA_RESTART | SA_NOCLDSTOP;
+    sigemptyset(&sa.sa_mask);
+    sigaction(SIGCHLD, &sa, NULL);
 }
