b4070ee147da0ed1cf286a364036a506aaa5568d
wonyocho <dnjsdud767@gmail.com>
2025-03-12 12:09:25 +0900

    [Refac] SGCHLD 함수 private으로 변경, 중괄호 컨벤션 수정

--- include/CGI/CgiExecuter.hpp
+++ include/CGI/CgiExecuter.hpp
@@ -21,22 +21,24 @@ struct CgiRequestData {
 		: scriptPath(script), queryString(query), requestMethod(method), requestBody(body) {}
 };
 
-class CgiExecuter
-{
-	private:
-		void setEnvVariables(const CgiRequestData& requestData); // 환경 변수 설정 (요청 데이터 구조체 전달)
-		void setupBodyPipe(const std::string& requestBody); // 본문 데이터를 처리하는 파이프 설정
-		void executeChild(int pipefd[2], const CgiRequestData& requestData); // 자식 프로세스 실행 (요청 데이터 전달)
-		void checkChildStatus(pid_t pid); // 자식 프로세스 종료 상태 확인
+class CgiExecuter {
+private:
+	void setEnvVariables(const CgiRequestData& requestData); // 환경 변수 설정 (요청 데이터 구조체 전달)
+	void setupBodyPipe(const std::string& requestBody); // 본문 데이터를 처리하는 파이프 설정
+	void executeChild(int pipefd[2], const CgiRequestData& requestData); // 자식 프로세스 실행 (요청 데이터 전달)
+	void checkChildStatus(pid_t pid); // 자식 프로세스 종료 상태 확인
 
-	public:
-		CgiExecuter();
-		~CgiExecuter();
+	static void handleSigchld(int signo); // `SIGCHLD` 핸들러
+	void setupSigchldHandler(); // `SIGCHLD` 핸들러 설정
 
-		int executeCgiScript(
-			const std::string& scriptPath,
-			const std::string& queryString,
-			const std::string& requestMethod,
-			const std::string& requestBody
-		);
+public:
+	CgiExecuter();
+	~CgiExecuter();
+
+	int executeCgiScript(
+		const std::string& scriptPath,
+		const std::string& queryString,
+		const std::string& requestMethod,
+		const std::string& requestBody
+	);
 };
